d6cdc9e539abc0c3dbf6b79f7fc059edb3425436
pgp <pgp@gmx.it>
2021-12-14 10:12:25 +0100

    make isDataConnectionEnabled work also for API < 26

--- app/src/main/java/it/pgp/currenttoggles/utils/Misc.java
+++ app/src/main/java/it/pgp/currenttoggles/utils/Misc.java
@@ -1,20 +1,34 @@
 package it.pgp.currenttoggles.utils;
 
-import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 
-// https://stackoverflow.com/questions/12806709/how-to-tell-if-mobile-network-data-is-enabled-or-disabled-even-when-connected
+import java.lang.reflect.Method;
 
 public class Misc {
-    @TargetApi(Build.VERSION_CODES.O)
     public static boolean isDataConnectionEnabled(Context context) {
-        TelephonyManager tm = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
-        return tm.isDataEnabled();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            TelephonyManager tm = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+            return tm.isDataEnabled();
+        }
+        else {
+            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            try {
+                Class<?> cmClass = Class.forName(cm.getClass().getName());
+                Method method = cmClass.getDeclaredMethod("getMobileDataEnabled");
+                method.setAccessible(true);
+                return (Boolean)method.invoke(cm);
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
     }
 
     public static boolean isWifiEnabled(Context context) {
